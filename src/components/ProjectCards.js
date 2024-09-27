103431f70aae7b0aea1fd5e50658ee6c2612eea0
Sally E <sme418@nyu.edu>
2024-09-27 10:32:32 -0400

    updated projects

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -25,85 +25,78 @@ import Discord from "./images/Discord.png";
 // Visit Mira Project
 import VisitMira from "./images/VisitMira.png";
 
-// CREATE NEW SCREENSHOTS / MAKE THEM ALL THE SAME SIZE
+// MAKE ALL IMAGES THE SAME SIZE & CREATE GIFS OF THE SCREENSHOTS
 function ProjectCards() {
   return (
     <main className="PageWrapper">
       <div className="ProjectsSection" id="projects">
-      <h1 className="PageTitle">Projects</h1>
-      {/* Ktown4u Redesign */}
-        <div className="Project">
-          <div className="ProjectImage">
-          <NavLink to="/project/ktown4u-redesign">
-              <img
-                src={Ktown4uRedesign}
-                alt="Ktown4u screenshot"
-              ></img>
-            </NavLink>
-          </div>
-          <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Ktown4u Web Redesign</div>
-              <div className="ProjectDate">Summer 2024 - Present</div>
+        <h1 className="PageTitle">Projects</h1>
+        <div className="ProjectWrapper">
+          {/* Ktown4u Redesign */}
+          <div className="Project">
+            <div className="ProjectImage">
+              <NavLink to="/project/ktown4u-redesign">
+                <img src={Ktown4uRedesign} alt="Ktown4u screenshot"></img>
+              </NavLink>
             </div>
-            <div className="ProjectTextRight">
+            <div className="ProjectText">
+              <div className="ProjectTitle">Ktown4u Web Redesign</div>
               <div className="ProjectSummary">
                 Improving web users' shopping experiences
               </div>
-              <div className="ProjectCategory">UX Design, UI Design</div>
-            </div>
-          </div>
-        </div>
-        {/* Retail Horror Stories */}
-        <div className="Project">
-          <div className="ProjectImage">
-          <NavLink to="/project/retail-horror-stories">
-              <img
-                src={RetailHorrorStories}
-                alt="Retail Horror Stories screenshot"
-              ></img>
-            </NavLink>
-          </div>
-          <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Retail Horror Stories: Black Friday Edition</div>
-              <div className="ProjectDate">Spring 2023</div>
-            </div>
-            <div className="ProjectTextRight">
-              <div className="ProjectSummary">
-              A "Choose Your Own Adventure" game inspired by real retail workers' experiences
+              <div className="ProjectDateCategory">
+                <p>Summer 2024 - Present | UX Design, UI Design</p>
               </div>
-              <div className="ProjectCategory">Web Development, Game Development</div>
+            </div>
+          </div>
+
+          {/* Retail Horror Stories */}
+          <div className="Project">
+            <div className="ProjectImage">
+              <NavLink to="/project/retail-horror-stories">
+                <img
+                  src={RetailHorrorStories}
+                  alt="Retail Horror Stories screenshot"
+                ></img>
+              </NavLink>
+            </div>
+            <div className="ProjectText">
+              <div className="ProjectTitle">
+                Retail Horror Stories: Black Friday Edition
+              </div>
+              <div className="ProjectSummary">
+                A "Choose Your Own Adventure" game inspired by real retail
+                workers' experiences
+              </div>
+              <div className="ProjectDateCategory">
+                <p>Spring 2023 | Web Development, Game Development</p>
+              </div>
             </div>
           </div>
         </div>
+
         {/* Kirby's Corner */}
         <div className="Project">
           <div className="ProjectImage">
-          <NavLink to="/project/kirbys-corner">
-              <img
-                src={KirbysCorner}
-                alt="Kirby's Corner screenshot"
-              ></img>
+            <NavLink to="/project/kirbys-corner">
+              <img src={KirbysCorner} alt="Kirby's Corner screenshot"></img>
             </NavLink>
           </div>
           <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Kirby's Corner</div>
-              <div className="ProjectDate">Spring 2023</div>
-            </div>
-            <div className="ProjectTextRight">
-              <div className="ProjectSummary">
+            <div className="ProjectTitle">Kirby's Corner</div>
+            <div className="ProjectSummary">
               An interactive fanpage dedicated to the Kirby universe
-              </div>
-              <div className="ProjectCategory">Web Development</div>
+            </div>
+            <div className="ProjectDateCategory">
+              <p>Spring 2023 | Web Development</p>
             </div>
           </div>
         </div>
+
         {/* Eclipse Soundscapes */}
         <div className="Project">
           <div className="ProjectImage">
-          <NavLink to="/project/eclipse-soundscapes">
+            <NavLink to="/project/eclipse-soundscapes">
               <img
                 src={EclipseSoundscapes}
                 alt="Eclipse Soundscapes screenshot"
@@ -111,22 +104,21 @@ function ProjectCards() {
             </NavLink>
           </div>
           <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Eclipse Soundscapes</div>
-              <div className="ProjectDate">Winter 2023</div>
+            <div className="ProjectTitle">Eclipse Soundscapes</div>
+            <div className="ProjectSummary">
+              Enhancing the accessibility of a field researcher training page
+              for older adults
             </div>
-            <div className="ProjectTextRight">
-              <div className="ProjectSummary">
-              Enhancing the accessibility of a field researcher training page for older adults
-              </div>
-              <div className="ProjectCategory">UX Design, UI Design</div>
+            <div className="ProjectDateCategory">
+              Fall 2022 | UX Design, UI Design
             </div>
           </div>
         </div>
+
         {/* Miiverse Revival */}
         <div className="Project">
           <div className="ProjectImage">
-          <NavLink to="/project/miiverse-revival">
+            <NavLink to="/project/miiverse-revival">
               <img
                 src={MiiverseRevival}
                 alt="Miiverse Revival screenshot"
@@ -134,15 +126,13 @@ function ProjectCards() {
             </NavLink>
           </div>
           <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Miiverse Revival</div>
-              <div className="ProjectDate">Fall 2022</div>
+            <div className="ProjectTitle">Miiverse Revival</div>
+            <div className="ProjectSummary">
+              A "revival" of the now-defunct Nintendo social media site called
+              Miiverse
             </div>
-            <div className="ProjectTextRight">
-              <div className="ProjectSummary">
-              A "revival" of the now-defunct Nintendo social media site called Miiverse
-              </div>
-              <div className="ProjectCategory">Web Development</div>
+            <div className="ProjectDateCategory">
+              <p>Fall 2022 | Web Development</p>
             </div>
           </div>
         </div>
@@ -150,23 +140,18 @@ function ProjectCards() {
         {/* Discord File Compressor */}
         <div className="Project">
           <div className="ProjectImage">
-          <NavLink to="/project/discord-file-compressor">
-              <img
-                src={Discord}
-                alt="Discord File Compressor screenshot"
-              ></img>
+            <NavLink to="/project/discord-file-compressor">
+              <img src={Discord} alt="Discord File Compressor screenshot"></img>
             </NavLink>
           </div>
           <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Discord File Compressor</div>
-              <div className="ProjectDate">Fall 2022</div>
+            <div className="ProjectTitle">Discord File Compressor</div>
+            <div className="ProjectSummary">
+              An in-app compression feature for larger files sent through
+              Discord
             </div>
-            <div className="ProjectTextRight">
-              <div className="ProjectSummary">
-              An in-app compression feature for larger files sent through Discord
-              </div>
-              <div className="ProjectCategory">UX Design, UI Design</div>
+            <div className="ProjectDateCategory">
+              <p>Fall 2022 | UX Design, UI Design</p>
             </div>
           </div>
         </div>
@@ -174,23 +159,17 @@ function ProjectCards() {
         {/* Visit Mira */}
         <div className="Project">
           <div className="ProjectImage">
-          <NavLink to="/project/visit-mira">
-              <img
-                src={VisitMira}
-                alt="Visit Mira screenshot"
-              ></img>
+            <NavLink to="/project/visit-mira">
+              <img src={VisitMira} alt="Visit Mira screenshot"></img>
             </NavLink>
           </div>
           <div className="ProjectText">
-            <div className="ProjectTextLeft">
-              <div className="ProjectTitle">Visit Mira</div>
-              <div className="ProjectDate">Fall 2021</div>
-            </div>
-            <div className="ProjectTextRight">
-              <div className="ProjectSummary">
+            <div className="ProjectTitle">Visit Mira</div>
+            <div className="ProjectSummary">
               We're stuck on a different planet - by choice
-              </div>
-              <div className="ProjectCategory">Web Development</div>
+            </div>
+            <div className="ProjectDateCategory">
+              <p>Fall 2021 | Web Development</p>
             </div>
           </div>
         </div>
