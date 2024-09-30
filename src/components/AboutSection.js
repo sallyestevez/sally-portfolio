4170e7bc779b03832144c75fb1e89eeee2b87337
Sally E <sme418@nyu.edu>
2024-09-29 20:44:27 -0400

    Fixed About page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,36 +2,33 @@ import React from "react";
 
 function AboutSection() {
   return (
-    <main className="PageWrapper">
-      <div className="AboutDiv">
-        <div className="AboutText">
-          <p>Hey, I'm Sally!</p>
-          <br></br>
-          <p>I'm a product designer and front-end developer based in NYC.</p>
-          <br></br>
-          <p>
-            I love blending my passion for problem-solving with creativity, and
-            I hope to use my expertise in design and development to craft
-            experiences that make people's lives easier.
-          </p>
-          <br></br>
-          <p>
-            I graduated from NYU Tandon's Integrated Design & Media program in
-            May 2023, but my learning journey hasn't ended. I'm always eager to
-            learn new things!
-          </p>
-          <br></br>
-          <p>
-            Feel free to chat with me about anything ranging from my hobbies (
-            <a href="https://rateyourmusic.com/~memelectraheart" target="blank">
-              music!
-            </a>{" "}
-            video games! reading! knitting!) to professional opportunities. :)
-          </p>
-          <br></br>
-        </div>
+    <div className="AboutDiv">
+      <div className="AboutText">
+        <p>Hey, I'm Sally!</p>
+        <br></br>
+        <p>I'm a product designer and front-end developer based in NYC.</p>
+        <br></br>
+        <p>
+          I love blending my passion for problem-solving with creativity, and I
+          hope to use my expertise in design and development to craft
+          experiences that make people's lives easier.
+        </p>
+        <br></br>
+        <p>
+          I graduated from NYU Tandon's Integrated Design & Media program in May
+          2023, but my learning journey hasn't ended. I'm always eager to learn
+          new things!
+        </p>
+        <br></br>
+        <p>
+          Feel free to chat with me about anything ranging from my hobbies (
+          <a href="https://rateyourmusic.com/~memelectraheart" target="blank">
+            music!
+          </a>{" "}
+          video games! reading! knitting!) to professional opportunities. :)
+        </p>
       </div>
-    </main>
+    </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,28 +2,25 @@ import React from "react";
 
 function Contact() {
   return (
-    <main className="PageWrapper">
-      <div className="AboutDiv">
-        <div className="AboutText">
-          <h1 className="PageTitle">Contact</h1>
-          <a href="mailto:sme418@nyu.edu">
-            <p>sme418@nyu.edu</p>
-          </a>
-          <a href="https://www.linkedin.com/in/sally-estevez/" target="blank">
-            <p>LinkedIn ↗</p>
-          </a>
-          <a href="https://github.com/sallyestevez/" target="blank">
-            <p>GitHub ↗</p>
-          </a>
-          <a
-            href="https://drive.google.com/file/d/12sU3d_c3X0xYfOubHxaWudaQb4nPh4NW/view"
-            target="blank"
-          >
-            <p>Resume ↗</p>
-          </a>
-        </div>
+    <div className="AboutDiv">
+      <div className="AboutText">
+        <a href="mailto:sme418@nyu.edu">
+          <p>sme418@nyu.edu</p>
+        </a>
+        <a href="https://www.linkedin.com/in/sally-estevez/" target="blank">
+          <p>LinkedIn ↗</p>
+        </a>
+        <a href="https://github.com/sallyestevez/" target="blank">
+          <p>GitHub ↗</p>
+        </a>
+        <a
+          href="https://drive.google.com/file/d/12sU3d_c3X0xYfOubHxaWudaQb4nPh4NW/view"
+          target="blank"
+        >
+          <p>Resume ↗</p>
+        </a>
       </div>
-    </main>
+    </div>
   );
 }
 
