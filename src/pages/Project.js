b648fda1118d19373690c72fd1149452b62caad4
Sally E <sme418@nyu.edu>
2024-10-05 18:01:36 -0400

    fixed responsiveness on projects page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -15,31 +15,13 @@ function Project() {
         <div>
           <header className="project--header">
             <div className="project-header-text">
-              <h1 className="projectTitle">{projectData.title}</h1>
+              <h1 className="PageTitle">{projectData.title}</h1>
               <h2 className="projectBlurb">{projectData.blurb}</h2>
             </div>
           </header>
         </div>
         <section>
-          <div className="project--content">
-            {projectData.projectText.map((text, i) => {
-              const type = text.type;
-              switch (type) {
-                case "p":
-                  return <p key={i}>{text.data}</p>;
-                case "h1":
-                  return <h1 key={i}>{text.data}</h1>;
-                case "h2":
-                  return <h2 key={i}>{text.data}</h2>;
-                case "h3":
-                  return <h3 key={i}>{text.data}</h3>;
-                case "h4":
-                  return <h4 key={i}>{text.data}</h4>;
-                default:
-                  return <p key={i}>{text.data}</p>;
-              }
-            })}
-          </div>
+          <div className="project--content"></div>
         </section>
       </main>
     </>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,12 +4,13 @@ import ProjectCard from "../components/ProjectCard";
 import ProjectData from "../components/ProjectData";
 import Footer from "../components/Footer";
 
+// CHECK OTHER PORTFOLIOS TO SEE HOW THEY DISPLAY THEIR PROJECTS
+// TRY TO MAKE ALL PROJECT DIVS THE SAME HEIGHT
 // RESPONSIVE WEB PAGES
 // USE SAME FORMAT FOR 'OTHER WORK' PAGE
 
 function Projects() {
   const [item] = useState(ProjectData);
-
   return (
     <>
       <Header />
