4b3a0fdac585d756576a7d6200bfb2efb95450b0
Sally <sme418@nyu.edu>
2024-10-17 14:19:37 -0400

    Made changes to Eclipse Soundscapes case study

--- src/components/project-content/EclipseSoundscapesContent.js
+++ src/components/project-content/EclipseSoundscapesContent.js
@@ -46,10 +46,9 @@ function EclipseSoundscapesContent() {
             <div className="ProjectPageSubtitle">Eclipse Soundscapes</div>
             <h2>Supported by NASA's Space Science Education Consortium (NSSEC)</h2>
             <p>
-              The Eclipse Soundscapes Project delivers an accessible multi-sensory 
-              experience that allows everyone, including people who are visually 
-              impared and others who were unable to see the eclipse with their own eyes, 
-              to experience the event.
+            The Eclipse Soundscapes Project aims to provide an accessible multi-sensory experience, 
+            enabling individuals, especially those who are visually impaired or unable to witness 
+            the eclipse firsthand, to experience the event.
             </p>
           </div>
           <div className="ProjectDiv ProjectProblem">
@@ -60,15 +59,40 @@ function EclipseSoundscapesContent() {
           </div>
           <div className="ProjectDiv ProjectObservations">
             <div className="ProjectPageSubtitle">Initial Observations</div>
-            <h2></h2>
+            <p>We conducted a thorough assessment of the website to identify usability issues and areas 
+              for enhancement, focusing on how users interact with the interface and their overall experience.</p>
+            <h2>Loading</h2>
+            <p>✷ Long load time</p>
+            <h2>Navigation</h2>
+            <p>✷ The directory is only at the top</p>
+            <p>✷ "Back to top" is at the bottom of the section and still cuts off top</p>
+            <p>✷ Too much scrolling</p>
+            <p>✷ Contacts, Instagram, and Twitter are cut off by moon</p>
+            <p>✷ No link is attached to the "here" in the opening paragraph</p>
+            <h2>Images</h2>
+            <p>✷ Very small</p>
+            <p>✷ No consistent sizes</p>
+            <h2>Text</h2>
+            <p>✷ Everything uses the same font</p>
+            <p>✷ Text is the entire width of the page</p>
+            <h2>Sections</h2>
+            <p>✷ Not enough space in between</p>
+            <p>✷ Too much text</p>
+            <p>✷ No breaks</p>
+            <h2>Background</h2>
+            <p>✷ Night mode (moon icon) doesn't really do anything</p>
+            <h2>Content</h2>
+            <p>✷ Zooming in makes everything much larger (edited)</p>
+            <p>✷ Download on Apple App Store and Google Play are on separate lines</p>
+            <p>✷ Looks better on mobile</p>
+            <p>✷ Can't view Field Researcher page in Spanish</p>
           </div>
           <div className="ProjectDiv ProjectResearchPlan">
             <div className="ProjectPageSubtitle">User Research & Synthesis</div>
-            <h2>User Interviews</h2>
-            <p>
-              We conducted a total of 12 interviews with users aged 55+. Those interviews were broken down into two parts:
-            </p>
-            <p><strong>Part 1:</strong> 20 questions to learn about their overall experiences and website preferences, including</p>
+            <p>To understand the needs of our target demographic, we conducted a total of 12 in-depth interviews 
+              with users agged 55+. The interviews were broken down into two parts:</p>
+            <p><strong>Part 1:</strong> A series 20 questions designed to gather insights into their overall 
+            experiences and website preferences, such as</p>
             <em><p>What is your preferred method of browsing the internet?</p>
             <p>Is having an option to have text read aloud beneficial to you?</p>
             <p>
@@ -80,13 +104,14 @@ function EclipseSoundscapesContent() {
             <p>
               Do any other websites come to mind where you really liked the layout of them? Why did you like it?
             </p></em>
-            <p><strong>Part 2:</strong> Usability testing of the current design to observe how they navigate the website</p>
+            <p><strong>Part 2:</strong> Usability testing of the current design to observe how participants navigated 
+            the website</p>
             <h2>User Responses</h2>
             <p className="quote">
-              "Too many words / information"
+              "There are too many words and information"
             </p>
             <p className="quote">
-              "Looks too dark"
+              "The website looks too dark"
             </p>
             <p className="quote">
               "I like the big print/text on the website"
@@ -95,34 +120,36 @@ function EclipseSoundscapesContent() {
               "Add more color"
             </p>
             <p className="quote">
-              "Hard to get to top of page"
+              "It's hard to get to top of page"
             </p>
             <p className="quote">
-              "Navigation is too jumpy. Can't tell what comes before or after"
+              "Navigation is too jumpy. I can't tell what comes before or after"
             </p>
             <p className="quote">
-              "Text is too wide. It's like reading a newspaper."
+              "The text is too wide. It's like reading a newspaper"
             </p>
             <h2>User Classification</h2>
-            <p>
+            <p>From the interviews, we categorized user preferences as follows:</p>
+            <p>✷ 
               <strong>10 out of 12</strong> users prefer visual learning over text based
             </p>
-            <p>
+            <p>✷ 
               <strong>12 out of 12</strong> users would benefit from text-to-speech
             </p>
-            <p>
+            <p>✷ 
               <strong>7 out of 12</strong> users prefer a lighter color scheme of blue or white
             </p>
-            <p>
+            <p>✷ 
               <strong>7 out of 12</strong> users believe navigating the website takes too much time
             </p>
-            <p>
+            <p>✷ 
               <strong>6 out of 12</strong> want to see more audio/visual content (images, videos, audio)
             </p>
-            <p>
+            <p>✷ 
               <strong>6 out of 12</strong> believe the website can be organized better
             </p>
             <h2>User Persona</h2>
+            <p>We developed a user persona that represents our target demographic and its needs.</p>
             <p><strong>Name: </strong>George Patterson</p>
             <p><strong>Age: </strong>70</p>
             <p>
@@ -134,21 +161,21 @@ function EclipseSoundscapesContent() {
             <div className="ProjectPageSubtitle">Low-Fidelity Concepts</div>
             <h2>Wireframes</h2>
             <p>
-              Based on what we learned from the user interviews and user testing, we were now ready to start 
-              creating our wireframes. We split up sections of the page and each worked on a section.
+            Based on what we learned from the user interviews and user testing, we created low-fidelity 
+            wireframes, dividing sections of the page for collaborative design and feedback.
             </p>
-            <h2>Solutions</h2>
-            <p>We were able to establish our top solutions for our redesign, which are:</p>
-            <p>Color Palette</p>
-            <h2>Side Bar</h2>
-            <p>Side bar</p>
-            <h2>Back to Top</h2>
+            <h2>Design Solutions</h2>
+            <p>Our redesign solutions included:</p>
+            <p>Color Palette - implementing a lighter, more inviting color scheme</p>
+            <p>Sidebar Navigation - Enhancing accessibility and usability</p>
+            <p>Back to Top Button - Facilitating easier navigation</p>
           </div>
           <div className="ProjectDiv ProjectHiFi">
             <div className="ProjectPageSubtitle">
               Final Prototype
             </div>
-            <p>Based on our research and drawn conclusions, we were able to come up with our high fidelity prototype.
+            <p>Utilizing our research findings, we developed a high-fidelity prototype that incorporates user 
+              feedback and design improvements, enhancing overall usability for the target audience.
             </p>
           </div>
           <div className="FigmaPrototypeDiv">
@@ -161,6 +188,7 @@ function EclipseSoundscapesContent() {
           </div>
           <div className="ProjectDiv ProjectTakeaways">
             <div className="ProjectPageSubtitle">Future Plans / Next Steps</div>
+            <p>To ensure the redesign effectively meets user needs, we have outlined the following steps for moving forward:</p>
             <h2>1. User Testing</h2>
             <p>Gain user feedback on prototypes.</p>
             <h2>2. Revision</h2>
@@ -170,18 +198,20 @@ function EclipseSoundscapesContent() {
             <h2>4. Implementing</h2>
             <p>Update official site with the revisions.</p>
             <div className="ProjectPageSubtitle">Suggested Client Plan of Action</div>
+            <p>To effectively enhance the user experience and accessibility of the website, we recommend implemented the following 
+              phased approach:</p>
             <h2>Now</h2>
-            <p>Make navigation accessible on all parts of the page</p>
-            <p>Add a buffer zone for text</p>
+            <p>✷ <strong>Enhance Navigation:</strong> Ensure navigation is accessible on all parts of the page</p>
+            <p>✷ <strong>Text Buffer Zone:</strong> Add a buffer zone around text to improve readability</p>
             <h2>Next</h2>
-            <p>Add a light mode</p>
-            <p>Make image sizes consistent</p>
-            <p>Break up large sections of text</p>
+            <p>✷ <strong>Light Mode:</strong> Implement a light mode option for better visual comfort</p>
+            <p>✷ <strong>Consistent Image Sizes:</strong> Standardize image sizes throughout the size</p>
+            <p>✷ <strong>Text Structure:</strong> Break up large sections of text for improved readability</p>
             <h2>Later</h2>
-            <p>Record and add video guides/walkthroughs</p>
-            <p>Include more visuals</p>
-            <p>Add text to speech</p>
-            <p>Add different language options</p>
+            <p>✷ <strong>Video Guides:</strong> Record and integrate video guides/walkthroughs for user support</p>
+            <p>✷ <strong>Additional Visuals:</strong> Include more visuals to enrich the user experience</p>
+            <p>✷ <strong>Text-to-Speech:</strong> Add text to speech functionality for accessibility</p>
+            <p>✷ <strong>Language Options:</strong> Incorporate options for different languages to reach a broader audience</p>
           </div>
         </div>
       </section>
