19775552a199655cb30b29186a72664a346b79e6
Sally E <sme418@nyu.edu>
2024-11-14 13:14:00 -0500

    Started adding summary/project information to Red Velvet project page

--- src/pages/other-work-pages/RedVelvetProject.js
+++ src/pages/other-work-pages/RedVelvetProject.js
@@ -21,16 +21,64 @@ function RedVelvetProject() {
           <div className="ProjectDateCategory UIPage">
             {OtherWorkData[1].date} | {OtherWorkData[1].category}
           </div>
+          <div className="ProjectImage--ProjectPage">
+            <a href="https://www.figma.com/design/XOj8O6ZYMrsVKv4eGwnC73/Red-Velvet?m=auto&t=RIleRXTjjHsQrWaD-1">
+              <div className="ProjectLink">Access the Figma file!</div>
+            </a>
+          </div>
         </div>
-        <div className="ProjectImage--ProjectPage">
-          <img
-            src={OtherWorkData[1].img}
-            alt={`${OtherWorkData[1].title} collage`}
-          />
-        </div>
+        <section>
+          <div className="ProjectContent">
+            <div className="ProjectDiv ProjectDetails">
+              <div className="ProjectPageSubtitle">Project Details</div>
+              <p>
+                <strong>Role:</strong> Visual Designer
+              </p>
+              <p>
+                <strong>Duration:</strong> Winter 2023 - Spring 2024
+              </p>
+              <p>
+                <strong>Tools:</strong> Figma, various font websites
+              </p>
+            </div>
+            <div className="ProjectDiv ProjectDetails">
+              <div className="ProjectPageSubtitle">Overview</div>
+              <p>
+                With this typography challenge, I focused on creating unique
+                typographic styles tailored to songs by the K-Pop group Red
+                Velvet. Each design captured the mood and theme of the track,
+                and I aimed to align typography with emotional tone.
+              </p>
+              <p>
+                I was inspired by the unique typographic styles for songs by the
+                K-Pop group Billlie.
+              </p>
+              <div className="ProjectPageSubtitle">Process</div>
+              <p>
+                ✴ <strong>Research & Inspiration: </strong>
+              </p>
+              <p>
+                ✴ <strong>Design Execution: </strong>
+              </p>
+              <p>
+                ✴ <strong>Cohesion: </strong>
+              </p>
+              <p>
+                ✴{" "}
+                <strong>
+                  Presentation: All of the designs were compiled in a Figma
+                  file.
+                </strong>
+              </p>
+              <div className="ProjectPageSubtitle">Challenges & Solutions</div>
+              <p></p>
+              <div className="ProjectPageSubtitle">Takeaways</div>
+              <p></p>
+            </div>
+          </div>
+        </section>
         {/* INCLUDE BACKGROUND INFORMATION ABOUT PROJECT - CAN USE 
         INFO FROM RESUME DESCRIPTION */}
-        {/* SAVE FILES ON LAPTOP TO FIX FORMATTING */}
         <RedVelvetCards item={item} />
       </div>
       <Footer />
